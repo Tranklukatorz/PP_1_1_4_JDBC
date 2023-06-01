80fd7620b2323b244c2f223b188fd16594868d35
Sergh <serghbr@gmail.com>
2023-06-01 23:32:35 +0300

    изменения для соответствия с заданием.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,7 +5,6 @@ import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,27 +26,24 @@ public class Main {
         usrs.add(new User("Игорь", "Герасимов", (byte) 68));
         usrs.add(new User("Богдан", "Титомир", (byte) 35));
 
+
+
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
 
-        usrs.forEach(x -> userService.saveUser(x.getName(), x.getLastName(), x.getAge()));
-
-        userService.removeUserById(4);
-        userService.removeUserById(1);
-
+        for (User user : usrs) {
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+            System.out.printf("User с именем – %s добавлен в базу данных\n", user.getName());
+        }
+        System.out.println("\n");
+        System.out.printf("%-4s %-8s %-10s %-3s\n","ID","Имя","Фамилия","Возраст" );
         List<User> usrList = userService.getAllUsers();
 
-        System.out.printf("%-4s %-10s %-10s %-5s\n","ИД","Имя","Фамилия","Возраст");
-        usrList.forEach(x -> System.out.printf("%-4d %-10s %-10s %-5s\n",
-                x.getId(),x.getName(),x.getLastName(), x.getAge()));
+        usrList.forEach(System.out::println);
 
         userService.cleanUsersTable();
 
-        System.out.println("---------------------------");
-
-        List<User> usrList2 = userService.getAllUsers();
-        usrList2.forEach(x -> System.out.printf("%d %.10s %.7s %.7s\n",
-                x.getId(),x.getName(),x.getLastName(), x.getAge()));
+        userService.dropUsersTable();
 
         Util.closeConnection();
 
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -19,7 +19,6 @@ public class User {
     private Byte age;
 
     public User() {
-
     }
 
     public User(String name, String lastName, Byte age) {
@@ -58,5 +57,10 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-4d %-8s %-10s %-3d", id, name, lastName, age);
     }
 }
